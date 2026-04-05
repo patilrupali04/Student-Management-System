d3e7d87c23fbae6ad467aecd8fd1004be53deccc
Rupali Patil <125479555+patilrupali04@users.noreply.github.com>
2026-04-05 21:14:21 +0530

    Add files via upload
    i have used sql server and entity framework and testing done with the postman

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Data
+{
+    public class AppDbContext :IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Student { get; set;  }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Course { get; set; } = string.Empty;
+
+
+        public DateTime CreateDate { get; set; }
+    }
+}
